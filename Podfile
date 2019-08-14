f361d2104d846c34cba227458c697a504f88c09c
Smalla <smallerquan@gmail.com>
2019-08-14 10:26:04 +0800

    初始化Pod配置

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform:ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'SASpecs' do
+
+  use_frameworks!
+  pod 'SnapKit'
+  pod 'Then'
+  pod 'Toast'
+
+end
---
+++ SASpecs/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SASpecs
+//
+//  Created by Smalla on 2019/8/1.
+//  Copyright © 2019 SASpecs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
